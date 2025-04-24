83b049c37863825ef93a9348d10ea816580c9334
Robert Berg <rberg@cps.edu>
2025-04-24 11:14:38 -0500

    Updated UCI data file with some additional print statements and fixed errors.

--- a8.py
+++ a8.py
@@ -1,6 +1,16 @@
 from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+xor_training_data = [
+    ([1, 1], [0]), 
+    ([1, 0], [1]), 
+    ([0, 1], [1]), 
+    ([0, 0], [0])
+    ]
+
+xorn = NeuralNet(2, 2, 1)
+xorn.train(xor_training_data, learning_rate=.7, iters=10000, print_interval=1000)
+print(xorn.test_with_expected(xor_training_data))
 
 
 
