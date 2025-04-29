aeda68d5721444e36012c06ba881d403ae5ddb74
Robert Berg <rberg@cps.edu>
2025-04-29 10:03:37 -0500

    included train test split

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 from neural import *
+from sklearn.model_selection import train_test_split
 
 
 def parse_line(line: str) -> Tuple[List[float], List[float]]:
@@ -14,6 +15,7 @@ def parse_line(line: str) -> Tuple[List[float], List[float]]:
     tokens = line.split(",")
     out = int(tokens[0])
     output = [0 if out == 1 else 0.5 if out == 2 else 1]
+    
 
     inpt = [float(x) for x in tokens[1:]]
     return (inpt, output)
@@ -48,16 +50,24 @@ def normalize(data: List[Tuple[List[float], List[float]]]):
 with open("wine_data.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
-# for line in training_data:
+train_data_split, test_data_split = train_test_split(training_data)
+
+# for line in train_data_split:
 #     print(line)  
 
-td = normalize(training_data)
+# print(len(training_data))
+# print(len(train_data_split))
+# print(len(test_data_split))
 
-# for line in td:
+# td = normalize(training_data)
+train_data_norm = normalize(train_data_split)
+test_data_norm = normalize(test_data_split)
+
+# for line in test_data_norm:
 #     print(line)
 
 nn = NeuralNet(13, 3, 1)
-nn.train(td, iters=10000, print_interval=1000, learning_rate=0.1)
+nn.train(train_data_norm, iters=10000, print_interval=1000, learning_rate=0.5)
 
-for i in nn.test_with_expected(td):
+for i in nn.test_with_expected(test_data_norm):
     print(f"desired: {i[1]}, actual: {i[2]}")
